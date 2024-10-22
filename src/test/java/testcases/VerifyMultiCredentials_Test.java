de13a909f174ebec02174574f2a8b6b084765612
aditya com <aditya com@DESKTOP-BDOOC5C>
2024-10-22 14:17:11 +0530

    22nd october

--- src/test/java/testcases/VerifyMultiCredentials_Test.java
+++ src/test/java/testcases/VerifyMultiCredentials_Test.java
@@ -28,7 +28,7 @@ public class VerifyMultiCredentials_Test extends TestBase
 		return new Object[][]
 				{
 				{"standard_user","secret_sauce"},
-				{"locked_out_user","secret_sauce"},
+				//{"locked_out_user","secret_sauce"},
 				{"problem_user","secret_sauce"},
 				{"performance_glitch_user","secret_sauce"},
 				{"error_user","secret_sauce"},
